7d6c2e6afcbbfd4c92d0be4b38800b756cfe1fca
mgackowski <mwgackowski@gmail.com>
2023-11-14 20:59:10 +0000

    Added Acorn model using WireframeRenderer I wrote previously

---
+++ Assets/Scripts/FloatInPlace.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/**
+ * A simple script to make an object float up and down using a sine function.
+ */
+public class FloatInPlace : MonoBehaviour
+{
+    [SerializeField] float frequency = 1f;
+    [SerializeField] float amplitude = 0.2f;
+
+    Vector3 originalPosition;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+    }
+    void Update()
+    {
+        Vector3 newPosition = originalPosition;
+        newPosition.y += Mathf.Sin(Time.time * frequency) * amplitude;
+        transform.position = newPosition;
+    }
+}
