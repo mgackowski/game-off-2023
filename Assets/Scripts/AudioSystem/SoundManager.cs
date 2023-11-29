75562e493d37ba030a148da09ca65b4d90d7362f
mgackowski <mwgackowski@gmail.com>
2023-11-29 18:26:55 +0000

    Phone sounds

--- Assets/Scripts/AudioSystem/SoundManager.cs
+++ Assets/Scripts/AudioSystem/SoundManager.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using Yarn.Unity;
 
 [DefaultExecutionOrder(-1)]
 public class SoundManager : MonoBehaviour {
     private static SoundManager Instance;
 
+    [SerializeField] private List<AudioClip> clips;
     [SerializeField] private AudioMixerGroup output;
 
     private List<AudioSource> sources = new List<AudioSource>();
@@ -16,6 +18,35 @@ public class SoundManager : MonoBehaviour {
         } else {
             Destroy(gameObject);
             return;
+        }
+    }
+
+    [YarnCommand("playSound")]
+    public static void PlayFromList(int index, bool looped)
+    {
+        for (int i = 0; i < Instance.sources.Count; i++)
+        {
+            var source = Instance.sources[i];
+            if (!source.isPlaying)
+            {
+                source.clip = Instance.clips[index];
+                source.loop = looped;
+                source.Play();
+                return;
+            }
+        }
+    }
+
+    [YarnCommand("stopSound")]
+    public static void StopFromList(int index)
+    {
+        for (int i = 0; i < Instance.sources.Count; i++)
+        {
+            var source = Instance.sources[i];
+            if (source.clip == Instance.clips[index] && source.isPlaying)
+            {
+                source.Stop();
+            }
         }
     }
 
